90e520daec63e774c04ae3fbfd78d2032aa59457
MiguelDzn2 <131978248+MiguelDzn2@users.noreply.github.com>
2025-04-26 16:41:01 +0100

    separar
    separar

---
+++ geometry/brandoninstrument.py
@@ -0,0 +1,15 @@
+from geometry.geometry import Geometry
+import numpy as np
+
+
+class BrandonGeometry(Geometry):
+    def __init__(self, width=1, height=1, depth=1, verticesAgogo=[], uv_data=[]):
+        super().__init__()
+
+        # Cada lado consiste em dois triângulos
+        position_data = verticesAgogo
+        
+        # Usa as coordenadas UV fornecidas
+        self.add_attribute("vec3", "vertexPosition", position_data)
+        self.add_attribute("vec2", "vertexUV", uv_data)
+        self.count_vertices() 
--- projeto.py
+++ projeto.py
@@ -9,6 +9,9 @@ from core_ext.mesh import Mesh
 from core_ext.renderer import Renderer
 from core_ext.scene import Scene
 from geometry.miguelinstrument import MiguelGeometry
+from geometry.zeinstrument import ZeGeometry
+from geometry.anainstrument import AnaGeometry
+from geometry.brandoninstrument import BrandonGeometry
 from extras.axes import AxesHelper
 from extras.grid import GridHelper
 from extras.movement_rig import MovementRig
@@ -47,19 +50,49 @@ class Example(Base):
         self.camera_rig.add(self.camera)
         self.camera_rig.set_position([0.5, 1, 10])
         self.scene.add(self.camera_rig)
-        
-        # Load and create the Agogo object with metal texture
-        positions, uvs = my_obj_reader2("geometry/miguelOBJ.obj")
-        geometry = MiguelGeometry(1, 1, 1, positions, uvs)
+
+        # Common texture for now
         metal_texture = Texture(file_name="images/miguelJPG.jpg")
         material = TextureMaterial(texture=metal_texture)
-        self.mesh = Mesh(geometry, material)
         
-        # Set up object rig for movement
-        self.object_rig = MovementRig()
-        self.object_rig.add(self.mesh)
-        self.object_rig.set_position([0, 0, 0])
-        self.scene.add(self.object_rig)
+        # Load Miguel's object (using the same OBJ for now)
+        positions_miguel, uvs_miguel = my_obj_reader2("geometry/miguelOBJ.obj") 
+        geometry_miguel = MiguelGeometry(1, 1, 1, positions_miguel, uvs_miguel)
+        self.mesh_miguel = Mesh(geometry_miguel, material)
+        self.object_rig_miguel = MovementRig()
+        self.object_rig_miguel.add(self.mesh_miguel)
+        self.object_rig_miguel.set_position([-3, 0, 0]) # Position Miguel
+        self.scene.add(self.object_rig_miguel)
+
+        # Load Ze's object (using the same OBJ for now)
+        positions_ze, uvs_ze = my_obj_reader2("geometry/miguelOBJ.obj") # TODO: Replace with zeOBJ.obj later
+        geometry_ze = ZeGeometry(1, 1, 1, positions_ze, uvs_ze) # Use ZeGeometry
+        self.mesh_ze = Mesh(geometry_ze, material)
+        self.object_rig_ze = MovementRig()
+        self.object_rig_ze.add(self.mesh_ze)
+        self.object_rig_ze.set_position([-1, 0, 0]) # Position Ze
+        self.scene.add(self.object_rig_ze)
+
+        # Load Ana's object (using the same OBJ for now)
+        positions_ana, uvs_ana = my_obj_reader2("geometry/miguelOBJ.obj") # TODO: Replace with anaOBJ.obj later
+        geometry_ana = AnaGeometry(1, 1, 1, positions_ana, uvs_ana) # Use AnaGeometry
+        self.mesh_ana = Mesh(geometry_ana, material)
+        self.object_rig_ana = MovementRig()
+        self.object_rig_ana.add(self.mesh_ana)
+        self.object_rig_ana.set_position([1, 0, 0]) # Position Ana
+        self.scene.add(self.object_rig_ana)
+
+        # Load Brandon's object (using the same OBJ for now)
+        positions_brandon, uvs_brandon = my_obj_reader2("geometry/miguelOBJ.obj") # TODO: Replace with brandonOBJ.obj later
+        geometry_brandon = BrandonGeometry(1, 1, 1, positions_brandon, uvs_brandon) # Use BrandonGeometry
+        self.mesh_brandon = Mesh(geometry_brandon, material)
+        self.object_rig_brandon = MovementRig()
+        self.object_rig_brandon.add(self.mesh_brandon)
+        self.object_rig_brandon.set_position([3, 0, 0]) # Position Brandon
+        self.scene.add(self.object_rig_brandon)
+
+        # Keep track of the currently controlled object rig
+        self.active_object_rig = self.object_rig_miguel 
         
         axes = AxesHelper(axis_length=2)
         self.scene.add(axes)
@@ -76,30 +109,31 @@ class Example(Base):
         self.camera_rig.update(self.input, self.delta_time)
         
         # Object movement with arrow keys and other controls
+        # TODO: Add logic to switch between active objects (e.g., number keys 1-4)
         move_amount = 2 * self.delta_time
         rotate_amount = 1 * self.delta_time
         
-        # Translation with arrow keys
+        # Translation with arrow keys affects the active object
         if self.input.is_key_pressed('left'):
-            self.object_rig.translate(-move_amount, 0, 0)
+            self.active_object_rig.translate(-move_amount, 0, 0)
         if self.input.is_key_pressed('right'):
-            self.object_rig.translate(move_amount, 0, 0)
+            self.active_object_rig.translate(move_amount, 0, 0)
         if self.input.is_key_pressed('up'):
-            self.object_rig.translate(0, 0, -move_amount)
+            self.active_object_rig.translate(0, 0, -move_amount)
         if self.input.is_key_pressed('down'):
-            self.object_rig.translate(0, 0, move_amount)
+            self.active_object_rig.translate(0, 0, move_amount)
             
-        # Rotation with UO
+        # Rotation with UO affects the active object
         if self.input.is_key_pressed('u'):
-            self.object_rig.rotate_y(rotate_amount)
+            self.active_object_rig.rotate_y(rotate_amount)
         if self.input.is_key_pressed('o'):
-            self.object_rig.rotate_y(-rotate_amount)
+            self.active_object_rig.rotate_y(-rotate_amount)
             
-        # Tilt with KL
+        # Tilt with KL affects the active object
         if self.input.is_key_pressed('k'):
-            self.object_rig.rotate_x(rotate_amount)
+            self.active_object_rig.rotate_x(rotate_amount)
         if self.input.is_key_pressed('l'):
-            self.object_rig.rotate_x(-rotate_amount)
+            self.active_object_rig.rotate_x(-rotate_amount)
         
         self.renderer.render(self.scene, self.camera)
 
